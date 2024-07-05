222fa3db0d9586ed4035c3958f693ea734688d3f
KJ Monahan <kj@forumone.com>
2024-07-05 17:15:17 -0500

    Add example components for a detail page with images and paragraphs.

---
+++ starterkits/drupal/examples/_paragraphs/AllParagraphsFragment.ts
@@ -0,0 +1,20 @@
+import { graphql } from '@/types/drupal/__generated__';
+
+/**
+ * Fragment for all potential paragraph types we want to retrieve if they're
+ * included (which typically means all paragraph types.) If you are using the
+ * Paragraphs module, you'll want to copy this file but replace the fragments
+ * with the fragments for your paragraphs.
+ */
+const AllParagraphsFragment = graphql(`
+  fragment AllParagraphsFragment on ParagraphUnion {
+    __typename
+    ... on ParagraphInterface {
+      id
+    }
+    ...QuoteParagraphFragment
+    ...TriviaParagraphFragment
+  }
+`);
+
+export default AllParagraphsFragment;
---
+++ starterkits/drupal/examples/_paragraphs/QuoteParagraph.tsx
@@ -0,0 +1,38 @@
+import Quote from '@/source/03-components/Quote/Quote';
+import {
+  FragmentType,
+  getFragmentData,
+  graphql,
+} from '@/types/drupal/__generated__';
+
+/**
+ * Fragment for a quote paragraph with just two plain text fields,
+ * a quote and a citation.
+ */
+const QuoteParagraphFragment = graphql(`
+  fragment QuoteParagraphFragment on ParagraphQuote {
+    quote
+    citation
+  }
+`);
+
+/**
+ * Component to display the quote. In this example, you would also need a
+ * Quote component in source that displays the quote with whatever styling you
+ * needed.
+ *
+ * Think of this as a paragraph--quote.html.twig template.
+ */
+function QuoteParagraph({
+  paragraph,
+}: {
+  paragraph: FragmentType<typeof QuoteParagraphFragment>;
+}) {
+  const quoteParagraph = getFragmentData(QuoteParagraphFragment, paragraph);
+  return (
+    <Quote content={quoteParagraph.quote} citation={quoteParagraph.citation} />
+  );
+}
+
+export default QuoteParagraph;
+export { QuoteParagraphFragment };
