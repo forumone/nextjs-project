18b5900446b76dfaebd415645b9b13849c340be9
KJ Monahan <kj@forumone.com>
2024-07-11 16:25:04 -0500

    Move CMS-specific utils and types into starter kit as well
    Prevents type errors from dependencies that aren't installed.

--- lib/drupal/generate-graphql-types.ts
+++ lib/drupal/generate-graphql-types.ts
@@ -1,12 +1,18 @@
+// The GraphQL codegen module is only installed on headless projects using
+// GraphQL (namely, Drupal), which is also the only type of project this script
+// applies to.
+// @ts-ignore
 import { CodegenContext, generate } from '@graphql-codegen/cli';
 import { resolve } from 'node:path';
-// The codegen file won't be at its location until setup-drupal is run. Ignore
-// the missing file, because @ts-expect-error then errors once setup-drupal is
-// run and the file *is* there.
+// The codegen and util files won't be at their destination until the setup
+// script is run. Using ts-ignore here because ts-expect-error will then error
+// once the script is run and the files *do* exist.
 // @ts-ignore
-import codegenConfig from '../../codegen';
-import drupal from '../../util/drupal/drupal';
-import graphqlEndpoint from '../../util/drupal/graphqlEndpoint';
+import codegenConfig from '@/codegen';
+// @ts-ignore
+import drupal from '@/util/drupal';
+// @ts-ignore
+import graphqlEndpoint from '@/util/drupal/graphqlEndpoint';
 
 async function generateGraphQLTypes() {
   try {
--- starterkits/drupal/app/[...slug]/page.tsx
+++ starterkits/drupal/app/[...slug]/page.tsx
@@ -8,8 +8,8 @@ import {
   canShowEntity,
   entityExists,
   routeIsInternal,
-} from '@/util/drupal/dataIsEntityType';
-import query from '@/util/drupal/query';
+} from '@/util/dataIsEntityType';
+import query from '@/util/query';
 import { draftMode } from 'next/headers';
 import { notFound } from 'next/navigation';
 import BasicPageFull from '../_components/content/BasicPageFull';
