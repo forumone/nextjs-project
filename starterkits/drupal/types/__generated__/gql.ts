7dee70d1fd8cf6eca102358e69b6078a15504b9d
KJ Monahan <kj@forumone.com>
2024-07-12 16:22:24 -0500

    Add starter templates for dynamic card paragraph and card view mode

--- starterkits/drupal/types/__generated__/gql.ts
+++ starterkits/drupal/types/__generated__/gql.ts
@@ -14,7 +14,9 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  */
 const documents = {
     "\n  query GetNodeByPath($path: String!) {\n    route(path: $path) {\n      __typename\n      ... on RouteInternal {\n        entity {\n          __typename\n          ... on NodeInterface {\n            status\n          }\n          ...BasicPageFragment\n        }\n      }\n    }\n  }\n": types.GetNodeByPathDocument,
+    "\n  fragment ArticleCardFragment on NodeArticle {\n    title\n    body {\n      summary\n    }\n    path\n    image {\n      variations(styles: LARGE) {\n        width\n        url\n        name\n        height\n      }\n      alt\n    }\n  }\n": types.ArticleCardFragmentFragmentDoc,
     "\n  fragment ArticleFullFragment on NodeArticle {\n    title\n    created {\n      timestamp\n    }\n    body {\n      processed\n    }\n  }\n": types.ArticleFullFragmentFragmentDoc,
+    "\n  fragment BasicPageCardFragment on NodePage {\n    title\n    body {\n      summary\n    }\n    path\n  }\n": types.BasicPageCardFragmentFragmentDoc,
     "\n  fragment BasicPageFragment on NodePage {\n    title\n    body {\n      processed\n    }\n  }\n": types.BasicPageFragmentFragmentDoc,
     "\n  fragment MenuItemFragment on MenuItem {\n    title\n    id\n    url\n    attributes {\n      class\n    }\n    internal\n  }\n": types.MenuItemFragmentFragmentDoc,
     "\n  fragment AccordionItemParagraphFragment on ParagraphAccordionItem {\n    id\n    accordionHeading\n    accordionBody {\n      processed\n    }\n  }\n": types.AccordionItemParagraphFragmentFragmentDoc,
@@ -23,6 +25,7 @@ const documents = {
     "\n  fragment BlockEmbedParagraphFragment on ParagraphBlockEmbed {\n    block {\n      ... on BlockInterface {\n        render\n      }\n    }\n  }\n": types.BlockEmbedParagraphFragmentFragmentDoc,
     "\n  fragment CardParagraphFragment on ParagraphCard {\n    id\n    cardLink {\n      url\n    }\n    cardMedia {\n      ... on MediaImage {\n        mediaImage {\n          alt\n          variations(styles: LARGE) {\n            height\n            url\n            width\n          }\n        }\n      }\n    }\n    cardSubtitle\n    cardTitle\n  }\n": types.CardParagraphFragmentFragmentDoc,
     "\n  fragment CardsParagraphFragment on ParagraphCards {\n    cards {\n      __typename\n      ... on ParagraphInterface {\n        id\n      }\n      ...CardParagraphFragment\n    }\n  }\n": types.CardsParagraphFragmentFragmentDoc,
+    "\n  fragment DynamicCardParagraphFragment on ParagraphDynamicCard {\n    cardReference {\n      __typename\n      ... on NodeInterface {\n        id\n        status\n      }\n      ...ArticleCardFragment\n      ...BasicPageCardFragment\n    }\n  }\n": types.DynamicCardParagraphFragmentFragmentDoc,
     "\n  fragment HeroParagraphFragment on ParagraphHero {\n    id\n    heroAlignment\n    heroBody {\n      processed\n    }\n    heroHasOverlay\n    heroHeading\n    heroImage {\n      ... on MediaImage {\n        id\n        name\n        mediaImage {\n          alt\n          height\n          width\n          url\n        }\n      }\n    }\n    heroLink {\n      title\n      url\n    }\n  }\n": types.HeroParagraphFragmentFragmentDoc,
     "\n  fragment WysiwygParagraphFragment on ParagraphWysiwyg {\n    wysiwyg {\n      processed\n    }\n  }\n": types.WysiwygParagraphFragmentFragmentDoc,
     "\n  query GetArticleByPath($path: String!) {\n    route(path: $path) {\n      __typename\n      ... on RouteInternal {\n        entity {\n          __typename\n          ... on NodeInterface {\n            status\n          }\n          ...ArticleFullFragment\n        }\n      }\n    }\n  }\n": types.GetArticleByPathDocument,
@@ -49,7 +52,15 @@ export function graphql(source: "\n  query GetNodeByPath($path: String!) {\n    
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
+export function graphql(source: "\n  fragment ArticleCardFragment on NodeArticle {\n    title\n    body {\n      summary\n    }\n    path\n    image {\n      variations(styles: LARGE) {\n        width\n        url\n        name\n        height\n      }\n      alt\n    }\n  }\n"): (typeof documents)["\n  fragment ArticleCardFragment on NodeArticle {\n    title\n    body {\n      summary\n    }\n    path\n    image {\n      variations(styles: LARGE) {\n        width\n        url\n        name\n        height\n      }\n      alt\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
 export function graphql(source: "\n  fragment ArticleFullFragment on NodeArticle {\n    title\n    created {\n      timestamp\n    }\n    body {\n      processed\n    }\n  }\n"): (typeof documents)["\n  fragment ArticleFullFragment on NodeArticle {\n    title\n    created {\n      timestamp\n    }\n    body {\n      processed\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  fragment BasicPageCardFragment on NodePage {\n    title\n    body {\n      summary\n    }\n    path\n  }\n"): (typeof documents)["\n  fragment BasicPageCardFragment on NodePage {\n    title\n    body {\n      summary\n    }\n    path\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
@@ -82,6 +93,10 @@ export function graphql(source: "\n  fragment CardParagraphFragment on Paragraph
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  fragment CardsParagraphFragment on ParagraphCards {\n    cards {\n      __typename\n      ... on ParagraphInterface {\n        id\n      }\n      ...CardParagraphFragment\n    }\n  }\n"): (typeof documents)["\n  fragment CardsParagraphFragment on ParagraphCards {\n    cards {\n      __typename\n      ... on ParagraphInterface {\n        id\n      }\n      ...CardParagraphFragment\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  fragment DynamicCardParagraphFragment on ParagraphDynamicCard {\n    cardReference {\n      __typename\n      ... on NodeInterface {\n        id\n        status\n      }\n      ...ArticleCardFragment\n      ...BasicPageCardFragment\n    }\n  }\n"): (typeof documents)["\n  fragment DynamicCardParagraphFragment on ParagraphDynamicCard {\n    cardReference {\n      __typename\n      ... on NodeInterface {\n        id\n        status\n      }\n      ...ArticleCardFragment\n      ...BasicPageCardFragment\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
