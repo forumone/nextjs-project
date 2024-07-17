3db5b0968da6bbd4a1829d482721f582ee742f02
Steve Clay <steve@mrclay.org>
2024-07-17 13:05:05 -0400

    More useful defaults for running WP for now

--- starterkits/wp/editor-styles/editor-styles.ts
+++ starterkits/wp/editor-styles/editor-styles.ts
@@ -3,7 +3,7 @@ import '@/source/00-config/mixins.css';
 import '@/source/01-global/fonts/fonts';
 import '@/source/01-global/index.css';
 
-import '@/app/_content';
+import '@/app/_content/blocks';
 import '@/app/_content/blocks/CustomBlockLibraryAwardWinner';
 import '@/app/_content/blocks/CustomBlockLibraryFeaturedFilms';
 import '@/app/_content/blocks/CustomBlockLibraryFeaturedTextList';
