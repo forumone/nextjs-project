d11253422ef7c5a762fca21d8f6dd8f4aafb0b35
Steve Clay <steve@mrclay.org>
2024-07-18 10:00:39 -0400

    Fix editor-styles paths

--- starterkits/wp/editor-styles/editor-styles.ts
+++ starterkits/wp/editor-styles/editor-styles.ts
@@ -3,10 +3,10 @@ import '@/source/00-config/mixins.css';
 import '@/source/01-global/fonts/fonts';
 import '@/source/01-global/index.css';
 
-import '@/app/_content/blocks';
-import '@/app/_content/blocks/CustomBlockLibraryAwardWinner';
-import '@/app/_content/blocks/CustomBlockLibraryFeaturedFilms';
-import '@/app/_content/blocks/CustomBlockLibraryFeaturedTextList';
-import '@/app/_content/blocks/CustomBlockLibraryFilmSelector';
+import '@/app/_components/blocks/core';
+import '@/app/_components/blocks/CustomBlockLibraryAwardWinner';
+import '@/app/_components/blocks/CustomBlockLibraryFeaturedFilms';
+import '@/app/_components/blocks/CustomBlockLibraryFeaturedTextList';
+import '@/app/_components/blocks/CustomBlockLibraryFilmSelector';
 
 import '@/source/06-utility/index.css';
